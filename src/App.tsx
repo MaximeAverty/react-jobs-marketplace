47195481386ecc3afd6ba0c2830adbe7a0e34a83
MaximeAverty <maxime.averty.pro@gmail.com>
2024-07-23 22:24:32 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from './components/ui/button';
+
+function App() {
+	return (
+		<div>
+			<h1>Hello World</h1>
+			<Button>Ouvrir la modal</Button>
+		</div>
+	);
+}
+
+export default App;
